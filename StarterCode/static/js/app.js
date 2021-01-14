bb487c72f160998fd5395e2135aa8b17a6ab066e
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-14 15:03:21 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,18 +1,43 @@
-let samplevalues = data.samples.map(info => info.sample_values);
-let otuID = data.samples.map(info => info.otu_ids);
-let otulabel = data.samples.map(info => info.otu_labels);
-// console.log(dates);
-console.log(samplevalues);
-console.log(otuID);
-console.log(otulabel);
-    
+// let samplevalues = data.samples.map(info => info.sample_values);
+// let otuID = data.samples.map(info => info.otu_ids);
+// let otulabel = data.samples.map(info => info.otu_labels);
 
+// console.log(samplevalues);
+// console.log(otuID);
+// console.log(otulabel);
+    
+// d3.select('#selDataset')
 
 d3.json("samples.json").then(function(data) {
     console.log(data);
-    d3.select('#selDataset')
-   
+    
+   var dropdown_id = data.samples.map(info => info.id);
+    console.log(dropdown_id);
+
+
+    var sel = d3.select('#selDataset');
+    
+
+    dropdown_id.forEach(function(otu_id) {
+      sel.append('option').property('value', otu_id).text(otu_id);
+    });
 });
+    
+
+    // Function to handle input change
+  function optionChange(otu_value) {
+    console.log('wow option change executed', otu_value);
+  }
+
+  
+    
+  
+
+
+  
+  
+
+  
 
 // let trace1 = {
 //     type: "bar",
@@ -26,4 +51,3 @@ d3.json("samples.json").then(function(data) {
 // };
  
 
-// Plotly.newPlot("bar", data, layout);
