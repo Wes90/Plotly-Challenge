0a3ef3d1541ffdc723198e8d53c15d60aeb7397e
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-15 15:35:36 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,26 +8,44 @@
     
 // d3.select('#selDataset')
 
+//Grab data
 d3.json("samples.json").then(function(data) {
     console.log(data);
     
-   var dropdown_id = data.samples.map(info => info.id);
-    console.log(dropdown_id);
+  var dropdown_id = data.samples.map(info => info.id);
+  console.log(dropdown_id);
+  
+  var datasamples = data.samples;
+  console.log(datasamples);
 
-
-    var sel = d3.select('#selDataset');
+  
+  var sel = d3.select('#selDataset');
     
 
     dropdown_id.forEach(function(otu_id) {
       sel.append('option').property('value', otu_id).text(otu_id);
     });
+
 });
-    
+
+var data_value = sel.property('value');
+var sel = d3.select('#selDataset');
 
     // Function to handle input change
-  function optionChange(otu_value) {
-    console.log('wow option change executed', otu_value);
+  function optionChanged(otu_value) {
+    var parse_otu_value = +otu_value;
+    console.log('otu value is', parse_otu_value);
+    if (parse_otu_value === parse_otu_value){
+      console.log('correct');
+
+    }
+    else {
+      console.log('error');
+    }
   }
+  var dataset = +data_value;
+  console.log(dataset);
+   
 
   
     
