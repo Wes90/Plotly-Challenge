da8d78f10b54518f17a5db49719b02e864281dee
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-17 20:19:50 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,43 +1,33 @@
-//Create optionChanged function
-function optionChanged(otu_value) {
-  var parse_otu_value = +otu_value;
-  console.log('otu value is', parse_otu_value);
-  if (parse_otu_value === parse_otu_value){
-    console.log('correct');
 
-  }
-  else {
-    console.log('error');
-  }
-}
- 
 // creating filter for top 10 otu's
 function filtertopOTUs(otu) {
-  return otu.otu_ids ;
+  return otu.otu_ids.slice();
 }
 
 
 
 //Grab data
+var belly_data;
+
 d3.json("samples.json").then(function(data) {
+  navel_data = data;
     console.log(data);
-//Initialize page with default plot
+// Initialize page with default plot
 function init(){
   test = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
+    x: [],
+    y: [] }];
 
   Plotly.newPlot("bar", test);
 }
-init(); 
-d3.selectAll("#selDataset").on("change", optionChanged);
-
+ d3.selectAll("#selDataset");
+ init();
 //Use filter() to pass the function as its argument
 var filteredotu = data.samples.filter(filtertopOTUs);
 // console.log(filteredotu);
 
 var datatest = filteredotu.map(info => info.sample_values);
-console.log(datatest);
+// console.log(datatest);
     
 var dropdown_id = data.samples.map(info => info.id);
 // console.log(dropdown_id);
@@ -53,15 +43,29 @@ var sel = d3.select('#selDataset');
 
 //Create variables
 
-let samplevalues = data.samples.map(info => info.sample_values);
-let otuID = data.samples.map(info => info.otu_ids);
-let otulabel = data.samples.map(info => info.otu_labels);
-
-
 
 });
+
  
+  //Create optionChanged function
+function optionChanged(otu_value) {
+  var parse_otu_value = +otu_value;
+  console.log('otu value is', parse_otu_value);
+  var id_data = navel_data.samples.filter(s => s.id == otu_value);
+  var otuID = id_data[0].otu_ids.slice(0,10);
+  console.log("top 10 otu's" ,otuID);
+  var samplevalue = id_data[0].sample_values.slice(0,10);
+  var otu_label
+  var samplevalues = data.samples.map(info => info.sample_values);
+  var samplevalues = data.samples.map(info => info.sample_values);
+
+};
+
+
   
+
+
+ 
   
    
 
