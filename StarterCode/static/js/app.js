700a543f17deb506ed154b4b87b0576212e01a3e
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-17 22:06:20 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,33 +1,11 @@
-
-// creating filter for top 10 otu's
-function filtertopOTUs(otu) {
-  return otu.otu_ids.slice();
-}
-
-
-
 //Grab data
-var belly_data;
+var naval_data;
 
 d3.json("samples.json").then(function(data) {
   navel_data = data;
+  
     console.log(data);
-// Initialize page with default plot
-function init(){
-  test = [{
-    x: [],
-    y: [] }];
 
-  Plotly.newPlot("bar", test);
-}
- d3.selectAll("#selDataset");
- init();
-//Use filter() to pass the function as its argument
-var filteredotu = data.samples.filter(filtertopOTUs);
-// console.log(filteredotu);
-
-var datatest = filteredotu.map(info => info.sample_values);
-// console.log(datatest);
     
 var dropdown_id = data.samples.map(info => info.id);
 // console.log(dropdown_id);
@@ -40,26 +18,49 @@ var sel = d3.select('#selDataset');
       sel.append('option').property('value', otu_id).text(otu_id);
     });
 
-
-//Create variables
-
-
+    d3.select("selDataset").on("change",optionChanged);
 });
 
- 
-  //Create optionChanged function
-function optionChanged(otu_value) {
-  var parse_otu_value = +otu_value;
-  console.log('otu value is', parse_otu_value);
-  var id_data = navel_data.samples.filter(s => s.id == otu_value);
+//Create optionChanged function
+function optionChanged(sub_id) {
+  // var parse_otu_value = +otu_value;
+  //Create variables
+  var id_data = navel_data.samples.filter(s => s.id == sub_id);
   var otuID = id_data[0].otu_ids.slice(0,10);
-  console.log("top 10 otu's" ,otuID);
-  var samplevalue = id_data[0].sample_values.slice(0,10);
-  var otu_label
-  var samplevalues = data.samples.map(info => info.sample_values);
-  var samplevalues = data.samples.map(info => info.sample_values);
+  var sampleValue = id_data[0].sample_values.slice(0,10);
+  var otuLabel = id_data[0].otu_labels.slice(0,10);
+  var otu_id_labels = String(otuID);
+  console.log('otu value is', sub_id);
+  console.log("top 10 otu's" ,otuID,sampleValue,otuLabel);
+  
+  var slicedData = id_data[0].sample_values.slice(0,10);
+  var reversedData = slicedData.reverse();
+  // var reversedData = otu_id_labels.reverse();
+  // Plot data
+  var trace1 = {
+    x:sampleValue,
+    y:reversedData,
+    type: "bar",
+    orientation: "h"
+  };
 
+  var data = [trace1];
+
+  var layout = {
+    // title: "Greek gods search results",
+    margin: {
+      l: 100,
+      r: 100,
+      t: 100,
+      b: 100
+    }
+  };
+  
+  Plotly.newPlot("bar", data, layout);
+  
 };
+ 
+
 
 
   
