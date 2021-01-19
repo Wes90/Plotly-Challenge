15e9ffd4c66216078aeb9203926172c12747fce7
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-18 21:35:06 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,11 @@
 //Grab data
-var naval_data;
 
+
+
+
+
+
+var navel_data;
 d3.json("samples.json").then(function(data) {
   navel_data = data;
   
@@ -10,15 +15,105 @@ d3.json("samples.json").then(function(data) {
 var dropdown_id = data.samples.map(info => info.id);
 // console.log(dropdown_id);
 
+// Landing bar graph
 var sel = d3.select('#selDataset');
-    
+
+var x_grab = navel_data.samples.map(s => s.sample_values);
+var y_grab = navel_data.samples.map(s => s.otu_ids);
+var label_grab = navel_data.samples.map(s => s.otu_labels);
+
+var first_sample_values = x_grab[0].slice(0,10);
+var first_otu_ids = y_grab[0].slice(0,10);
+var first_labels = label_grab[0].slice(0,10);
+ 
+var bar_test = ['wes', 'olivia', 'jacob', 'julia', 'bailey', 'shane', 'folsom', 'kate', 'greg', 'fox']
+var trace1 = {
+  x: first_sample_values,
+  y: bar_test,
+  type: "bar",
+  orientation: "h"
+};
+
+var data = [trace1];
+
+var layout = {
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
+Plotly.newPlot("bar", data, layout);
+
+// Landing bubble chart
+var trace1 = {
+  x: first_otu_ids,
+  y: first_sample_values,
+  text: first_labels,
+  mode: 'markers',
+  marker: {
+    size: first_sample_values,
+    color: first_otu_ids,
+  }
+};
+
+var data = [trace1];
+
+var layout = {
+  xaxis: { title: "OTU ID" },
+  showlegend: false,
+  height: 600,
+  width: 1200
+};
+
+Plotly.newPlot('bubble', data, layout);
+
+// Landing gauge chart
+var data = [
+  {
+    type: "indicator",
+    mode: "gauge+number+delta",
+    value: 420,
+    title: { text: "Speed", font: { size: 24 } },
+    delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+    gauge: {
+      axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
+      bar: { color: "darkblue" },
+      bgcolor: "white",
+      borderwidth: 2,
+      bordercolor: "gray",
+      steps: [
+        { range: [0, 250], color: "cyan" },
+        { range: [250, 400], color: "royalblue" }
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 490
+      }
+    }
+  }
+];
+
+var layout = {
+  width: 400,
+  height: 400,
+  margin: { t: 25, r: 25, l: 25, b: 25 },
+  paper_bgcolor: "lavender",
+  font: { color: "darkblue", family: "Arial" }
+};
+
+Plotly.newPlot('gauge', data, layout);  
+
+
 //Append data to dropdown menu
 
     dropdown_id.forEach(function(otu_id) {
       sel.append('option').property('value', otu_id).text(otu_id);
     });
 
-    d3.select("selDataset").on("change",optionChanged);
+  d3.select("selDataset").on("change",optionChanged);
 });
 
 //Create optionChanged function
@@ -29,20 +124,27 @@ function optionChanged(sub_id) {
   var otuID = id_data[0].otu_ids.slice(0,10);
   var sampleValue = id_data[0].sample_values.slice(0,10);
   var otuLabel = id_data[0].otu_labels.slice(0,10);
-  var otu_id_labels = String(otuID);
+  var demo_data = navel_data.metadata.filter(s => s.id == sub_id); 
+  console.log(demo_data);
   console.log('otu value is', sub_id);
   console.log("top 10 otu's" ,otuID,sampleValue,otuLabel);
+  var otu_string = (label) => 'OTU' + label;
+  console.log(otu_string(otuID));
   
-  
-  
+
   var slicedData = id_data[0].sample_values.slice(0,10);
   var reversedData = slicedData.reverse();
   // var reversedData = otu_id_labels.reverse();
 
+// Demographic info
+  var demo_sel = d3.select('#sample-metadata').append('ul');
+  demo_sel.append('li').text(demo_data[0][0]);
+  demo_sel.html("");
+
   // Bar chart
   var trace1 = {
     x:sampleValue,
-    y:otuID,
+    y: otu_string,
     type: "bar",
     orientation: "h"
   };
@@ -59,10 +161,12 @@ function optionChanged(sub_id) {
   };
   
   Plotly.newPlot("bar", data, layout);
+
 // Bubble chart
   var trace1 = {
     x: otuID,
     y: sampleValue,
+    text: otuLabel,
     mode: 'markers',
     marker: {
       size: sampleValue,
@@ -73,10 +177,10 @@ function optionChanged(sub_id) {
   var data = [trace1];
   
   var layout = {
-    title: 'Marker Size',
+    xaxis: { title: "OTU ID" },
     showlegend: false,
     height: 600,
-    width: 1100
+    width: 1200
   };
   
   Plotly.newPlot('bubble', data, layout);
@@ -109,14 +213,14 @@ function optionChanged(sub_id) {
   ];
   
   var layout = {
-    width: 500,
+    width: 400,
     height: 400,
     margin: { t: 25, r: 25, l: 25, b: 25 },
     paper_bgcolor: "lavender",
     font: { color: "darkblue", family: "Arial" }
   };
   
-  Plotly.newPlot('myDiv', data, layout);  
+  Plotly.newPlot('gauge', data, layout);  
 };
  
 
@@ -139,15 +243,5 @@ function optionChanged(sub_id) {
 
   
 
-// let trace1 = {
-//     type: "bar",
-//     x: samplevalues,
-//     y: otuID,
 
-// };
-// let data = [trace1];
-// let layout = {
-//     barmode: 'group'
-// };
- 
 
