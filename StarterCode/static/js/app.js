39461ef816324a1805b11c67d47d4c46aa9b7cb6
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-01-19 00:39:51 -0700

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,7 +11,7 @@ var dropdown_id = data.samples.map(info => info.id);
 
 // Landing bar graph
 var sel = d3.select('#selDataset');
-
+// Landing variables
 var x_grab = navel_data.samples.map(s => s.sample_values);
 var y_grab = navel_data.samples.map(s => s.otu_ids);
 var label_grab = navel_data.samples.map(s => s.otu_labels);
@@ -26,7 +26,7 @@ var first_otu_list =[]
   function myfunction(item){
     first_otu_list.push('OTU '+ item);
   }
-var bar_test = ['wes', 'olivia', 'jacob', 'julia', 'bailey', 'shane', 'folsom', 'kate', 'greg', 'fox']
+
 var trace1 = {
   x: first_sample_values,
   y: first_otu_list,
@@ -75,7 +75,7 @@ var data = [
     type: "indicator",
     mode: "gauge+number+delta",
     value: 420,
-    title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+    title: { text: "Washing Frequency", font: { size: 24 } },
     delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
     gauge: {
       axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
@@ -100,13 +100,13 @@ var layout = {
   width: 400,
   height: 400,
   margin: { t: 25, r: 25, l: 25, b: 25 },
-  paper_bgcolor: "lavender",
+  paper_bgcolor: "white",
   font: { color: "darkblue", family: "Arial" }
 };
 
 Plotly.newPlot('gauge', data, layout);  
 
-//Append data to dropdown menu
+// Append data to dropdown menu
 
     dropdown_id.forEach(function(otu_id) {
       sel.append('option').property('value', otu_id).text(otu_id);
@@ -115,10 +115,10 @@ Plotly.newPlot('gauge', data, layout);
   d3.select("selDataset").on("change",optionChanged);
 });
 
-//Create optionChanged function
+// Create optionChanged function
 function optionChanged(sub_id) {
   
-//Create variables
+// On change variables
   var id_data = navel_data.samples.filter(s => s.id == sub_id);
   var otuID = id_data[0].otu_ids.slice(0,10);
   var sampleValue = id_data[0].sample_values.slice(0,10);
@@ -145,7 +145,7 @@ function optionChanged(sub_id) {
   var demo_sel = d3.select('#sample-metadata').append('ul');
   demo_sel.append('li').text(demo_data[0][0]);
   demo_sel.html("");
-
+  
 // Bar chart
   var trace1 = {
     x:sampleValue,
@@ -196,7 +196,7 @@ function optionChanged(sub_id) {
       type: "indicator",
       mode: "gauge+number+delta",
       value: 420,
-      title: { text: "Speed", font: { size: 24 } },
+      title: { text: "Washing Frequency", font: { size: 24 } },
       delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
       gauge: {
         axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
